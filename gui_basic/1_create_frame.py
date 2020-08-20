2a40ee6966e479e95de6fe16e8901139f701a5a3
jazzrabbit <cloudkim2024@gmail.com>
2020-08-20 04:57:05 -0400

    Button practice

--- gui_basic/1_create_frame.py
+++ gui_basic/1_create_frame.py
@@ -3,5 +3,8 @@ from tkinter import *
 root = Tk()
 root.title("My GUI")
 root.geometry("640x480")
+#root.geometry("640x480+300+300")  #  w * h  + location coordinates 
+
+
 
 root.mainloop()
---
+++ gui_basic/2_button.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+root = Tk()
+root.title("My GUI tool")
+
+btn1 = Button(root, text="Button1")
+btn1.pack()
+
+btn2 = Button(root, padx=5, pady=10, text="Button22222222222222222")
+btn2.pack()
+
+btn3 = Button(root, padx=10, pady=5, text="Button2")  #상대적 설정 
+btn3.pack()
+
+btn4 = Button(root, width=10, height=3, text="Button444444444444") #버튼 크기 강제설정 
+btn4.pack()
+
+btn5 = Button(root, fg="red", bg="yellow", text="Button5")
+btn5.pack()
+
+photo = PhotoImage(file="gui_basic/bp_jisoo.png")
+btn6 = Button(root, image=photo)
+btn6.pack()
+
+
+root.mainloop()
