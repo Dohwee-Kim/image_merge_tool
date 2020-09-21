639783d07722cf48499bc3845e01ed6cc1c53b9a
jazzrabbit <cloudkim2024@gmail.com>
2020-09-20 20:21:28 -0400

    Layout Frame

--- gui_basic/image_merger/1_create_layout.py
+++ gui_basic/image_merger/1_create_layout.py
@@ -16,13 +16,23 @@ btn_del_file.pack(side="right")
 
 #List Frame 
 list_frame = Frame(root)
-list_frame.pack()
+list_frame.pack(fill="both")
 scrollbar = Scrollbar(list_frame)
 scrollbar.pack(side="right", fill="y")
 
 list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set) #max 15 files 
 list_file.pack(side="left", fill="both", expand=True)
 scrollbar.config(command=list_file.yview)
+
+
+#save directories 
+path_frame = LabelFrame(root, text="Save Folder")
+path_frame.pack()
+txt_dest_path = Entry(path_frame)
+txt_dest_path.pack(side="left", fill = "x", expand = True)
+
+btn_dest_path = Button(path_frame, text="Open", width = 10)
+btn_dest_path.pack(side="right")
 
 
 root.resizable(False,False)
